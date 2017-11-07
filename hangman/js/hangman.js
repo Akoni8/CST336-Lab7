e90d84a8bdd79db741920359d30f4f6156c59f6b
Akoni <hakmorrison@csumb.edu>
2017-11-07 23:31:56 +0000

    updated lab

--- hangman/js/hangman.js
+++ hangman/js/hangman.js
@@ -7,7 +7,9 @@ var selectedWord = "";
 var selectedHint = "";
 var board = "";
 var remainingGuesses = 6;
-var words = ["snake","monkey","beetle"];
+var words = [{word: "snake", hint: "It's a reptile"},
+             {word: "monkey", hint: "It's a mammal"},
+             {word: "beetle", hint: "It's an insect"}];
             
 //Begin game when page is fully loaded
 window.onload = startGame();
@@ -26,40 +28,55 @@ function initBoard() {
 }
 function pickWord(){
     var randomInt = Math.floor(Math.random() * words.length);
-    selectedWord = words[randomInt].toUpperCase();
+    selectedWord = words[randomInt].word.toUpperCase();
+    selectedHint = words[randomInt].hint;
 }
             
 function updateBoard() {
+    $("#word").empty();
+    
     for(var letter of board) {
     document.getElementById("word").innerHTML += letter + " ";
     }
+    
+    $("#word").append("<br />");
+    $("#word").append("<span class='hint'>Hint: " + selectedHint + "</span>");
 }
 
 function createLetters() {
     for(var letter of alphabet){
-        $("#letters").append("<button class='letter' id='" + letter + "'>" + letter + "</button>");
+        $("#letters").append("<button class='letter btn btn-success' id='" + letter + "'>" + letter + "</button>");
     }
 }
 
 $(".letter").click(function(){
     console.log($(this).attr("id"));
+    checkLetter($(this).attr("id"));
+    disableButton($(this));
 })
 
 function checkLetter(letter){
     var positions = new Array();
     
     for(var i = 0; i < selectedWord.length; i++) {
-        console.log(selectedWord);
         if(letter == selectedWord[i]) {
-            positions.push[i];
+            positions.push(i);
         }
     }
     
     if(positions.length > 0){
         updateWord(positions,letter);
+        
+        if(!board.includes('_')){
+            endGame(true);
+        }
     }
     else {
         remainingGuesses -=1;
+        updateMan();
+    }
+    if(remainingGuesses <= 0) {
+        endGame(false);
     }
 }
 
@@ -72,4 +89,27 @@ function updateWord(positions,letter){
 
 function replaceAt(str, index, value){
     return str.substr(0,index) + value + str.substr(index + value.length);
+}
+
+function updateMan() {
+    $("#hangImg").attr("src","img/stick_" + (6 - remainingGuesses) + ".png");
+}
+
+function endGame(win){
+    $("#letters").hide();
+    
+    if(win){
+        $('#won').show();
+    } else {
+        $('#lost').show();
+    }
+}
+
+$(".replayBtn").on("click", function(){
+    location.reload();
+});
+
+function disableButton(btn) {
+    btn.prop("disabled", true);
+    btn.attr("class", "btn btn-danger");
 }
